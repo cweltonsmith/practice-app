282641796fc87f6b0a074e52f2615cf570a4448e
cweltonsmith <c.welton.smith@gmail.com>
2019-09-19 12:22:09 -0500

    Finsihed App

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-const calculateGrandTotal = (vartotal, vartip, vartax) => {
-    return vartotal + vartip + vartax
-}
-
-const calculateTip = (vartotal, vartiprate) => {
-    return vartotal * (vartiprate/100)
-}
-
-const calculateTax = (vartotal, vartaxRate) => {
-    return vartotal * (vartaxRate/100)
-}
-
-
-document.querySelector('#calculate').addEventListener('click', () => {
-
+function calculations() {
+    
+    //Passes string and returns float
     const total = parseFloat(document.querySelector('#total-field').value)
     const tipRate = parseFloat(document.querySelector('#rate-field').value)
 
-    const tip = calculateTip(total, tipRate)
+    //Doing all the simple calculations
+    const tip = total * (tipRate/100)
     const taxRate = 5.5
-    const tax = calculateTax(total, taxRate)
-    const grandTotal = calculateGrandTotal(total, tip, vartax)
+    const tax = total * (taxRate/100)
+    const grandTotal = total + tip + tax
 
-    document.querySelector('#tip-amount').value = tip
-    document.querySelector('#tax-amount').value = tax
-    document.querySelector('#grand-total').value = grandTotal
-})
+    //Modifying HTML page with the calculations output
+    document.querySelector('#tip-amount').innerHTML = `$${tip.toFixed(2)}`
+    document.querySelector('#tax-amount').innerHTML = `$${tax.toFixed(2)}`
+    document.querySelector('#total-amount').innerHTML = `$${grandTotal.toFixed(2)}`
+}
+
+
+//On click perform the calculation function
+document.querySelector('#calculate').addEventListener('click', calculations)
